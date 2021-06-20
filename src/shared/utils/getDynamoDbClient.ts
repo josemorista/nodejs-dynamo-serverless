4495078fe3b5cf48050c69b645f7f04d99df8974
josemorista <josemorista@id.uff.br>
2021-06-20 20:13:27 -0300

    styles: added semi to getDynamoDbClient

--- src/shared/utils/getDynamoDbClient.ts
+++ src/shared/utils/getDynamoDbClient.ts
@@ -3,11 +3,11 @@ import { DynamoDB } from 'aws-sdk';
 const options = {
 	region: 'localhost',
 	endpoint: 'http://localhost:8000'
-}
+};
 
 export const getDynamoDbClient = () => {
 	if (process.env.isOffline) {
 		return new DynamoDB.DocumentClient(options);
 	}
 	return new DynamoDB.DocumentClient();
-}
+};
